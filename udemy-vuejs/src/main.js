bea0481a50343af6d0ae93139a182c68ad4d459d
Ryutanaka <tanakatennis57@gmail.com>
2023-03-03 20:23:56 +0900

    commit

--- udemy-vuejs/src/main.js
+++ udemy-vuejs/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+//LikeNumber.vueをインポートする
+import LikeNumber from './components/LikeNumber.vue'
 
 Vue.config.productionTip = false
+//LikeNumber.vueコンポーネントとしてグローバル登録
+Vue.component('LikeNumber', LikeNumber);
 
 new Vue({
   render: h => h(App),
